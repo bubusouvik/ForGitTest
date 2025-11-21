e16aa36c0a0c924882e4839bd962d607bc4f82ce
bubusouvik <bubusouvik1997@gmail.com>
2025-11-21 15:41:25 +0530

    added and modified

--- src/test/java/com/dummy/git/DummyGitTest.java
+++ src/test/java/com/dummy/git/DummyGitTest.java
@@ -1,6 +1,12 @@
 package com.dummy.git;
 
+import org.testng.annotations.Test;
+
 public class DummyGitTest {
 	
-	
+	@Test
+	public void gitTest() {
+		System.out.println("Sample test!!");
+		System.out.println("Sample test1!!");
+	}
 }
